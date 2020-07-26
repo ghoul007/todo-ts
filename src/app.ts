23376b17c55f2a237eed5a1513fe975284b3351c
ahmed <ahmed.elghoul@infor.com>
2020-07-26 15:28:52 +0100

    add routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
-let age: number;
-age = 30; 
+import express, { NextFunction, Request, Response } from 'express';
+import todoRoute from './routes/todos';
 
 
-console.log(age);
+
+const app = express();
+
+app.use('/todos', todoRoute);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({ message: err.message })
+})
+
+app.listen(3000);
